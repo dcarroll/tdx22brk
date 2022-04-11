6229bbb7ecc10c5dd4fe950a8d8fc9cb65809861
Dave Carroll <116254+dcarroll@users.noreply.github.com>
2022-04-11 16:04:47 -0700

    try a new implementation

--- clearData.sh
+++ clearData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sfdx force:apex:execute -f deleteAccount.apex -u tdxscratch-2
-sfdx force:apex:execute -f deleteContact.apex -u tdxscratch-2
-sfdx force:apex:execute -f deleteBankProduct.apex -u tdxscratch-2
-sfdx force:apex:execute -f deleteBankAccount.apex -u tdxscratch-2
+sfdx djc:cleardata -o Bank_Account__c -u bulktest
+sfdx djc:cleardata -o Bank_Product__c -u bulktest
+sfdx djc:cleardata -o Contact -u bulktest
+sfdx djc:cleardata -o Account -u bulktest
